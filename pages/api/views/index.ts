2966be4503745769b5c0cc1b10b8cf0aea500945
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-15 12:27:26 -0400

    Edit content and debug views unsuccessful

--- pages/api/views/index.ts
+++ pages/api/views/index.ts
@@ -10,8 +10,39 @@ export default async function handler(
       .selectFrom('views')
       .select(['slug', 'count'])
       .execute();
+    console.log('/api/views')
+    console.log({data})
+    console.log({data})
 
-    return res.status(200).json(data);
+    // data
+
+  // {
+  //   data: [
+  //     { slug: 'backend', count: 1 },
+  //     { slug: 'backend', count: 1 },
+  //     { slug: 'backend', count: 1 },
+  //     { slug: 'backend', count: 1 },
+  //     { slug: 'backend', count: 1 },
+  //     { slug: 'backend', count: 1 },
+  //     { slug: 'backend', count: 1 },
+  //     { slug: 'backend', count: 1 },
+  //     { slug: 'backend', count: 1 },
+  //     { slug: 'career', count: 1 },
+  //     { slug: 'career', count: 1 },
+  //     { slug: 'career', count: 1 },
+  //     { slug: 'hello-world', count: 1 },
+  //     { slug: 'hello-world', count: 1 },
+  //     { slug: 'career', count: 1 },
+  //     { slug: 'hello-world', count: 1 },
+  //     { slug: 'hello-world', count: 1 },
+  //     { slug: 'career', count: 1 },
+  //     { slug: 'career', count: 1 },
+  //     { slug: 'career', count: 1 }
+  //   ]
+  // }
+
+
+  return res.status(200).json(data);
   } catch (e) {
     console.log(e);
     return res.status(500).json({ message: e.message });
