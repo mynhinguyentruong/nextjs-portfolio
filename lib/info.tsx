3c9f0c3235e078536e2ebe8ce91b5a906f6db5f0
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-15 10:57:56 -0400

    Change aboutme page

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,18 +1,18 @@
 import me from '../app/placeholder.jpg';
 
-export const name = 'Your Name';
+export const name = 'Nhi Nguyen';
 export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm a developer who loves Next.js. I work at BigCo or CoolStartup as a Software Engineer.
+        Hey 🤘, I'm Nhi - currently looking for <span className="font-bold">open source projects</span> where I can help build a better future for web development.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      When I'm not coding, I'm probably playing video games or watching anime. I also love to travel and explore new places.
+        When I'm not coding, I'm probably dancing to Kpop or walking with my dog. I also love to travel and explore new places.
     </>
   );
 };
