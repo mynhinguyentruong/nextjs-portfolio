7a6e72e5abeaa9153059befeee40652c6ed37fc4
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-16 15:37:35 -0400

    Change aboutme page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,70 @@
+import type { Metadata } from 'next';
+import {
+    GitHubIcon,
+    YoutubeIcon,
+    ArrowIcon,
+    TwitterIcon,
+} from 'components/icons';
+
+export const metadata: Metadata = {
+    title: 'About',
+    description: 'A Developer that build cool stuffs and love Next.js.',
+};
+
 export default function AboutPage() {
-  return (
-    <section>
-      <h1 className="font-bold text-3xl font-serif">About Me</h1>
-      <p className="my-5 text-neutral-800 dark:text-neutral-200">
-        This is my about page.
-      </p>
-    </section>
-  );
+    return (
+        <section>
+            <h1 className="font-bold text-3xl font-serif">About Me</h1>
+            <p className="my-5 text-neutral-800 dark:text-neutral-200">
+                Hey, I'm Nhi.
+            </p>
+            <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200">
+                <p>
+                    I'm currently the <b>VP of Developer Experience at Vercel</b>, where I
+                    lead our Developer Relations and Documentation teams. I focus on{' '}
+                    <b>educating and growing</b> the Vercel and Next.js communities.
+                </p>
+                <hr />
+                <p>
+                    I like to try out different technology and build cool stuffs from it. I usually ask WTF is this bug and try all possible way to smash it.
+                </p>
+                <p>
+                    I <b>love</b> building for the web. From something as simple as a
+                    single HTML file – all the way to large Next.js applications. The web
+                    is incredible. Anyone can become a developer, writer, or creator – and
+                    no one has to ask for permission. You can just build.
+                </p>
+                <p className="mb-8">
+                    Beside coding, I try to find good companies to invest in longterm. I like to talk about long-term investment and how you can leverage your TFSA account so that maybe 10 years from now, you can withdraw money from it completely tax free!
+                </p>
+                <div className="flex flex-col gap-2 md:flex-row md:gap-2">
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://twitter.com/ymihn1"
+                        className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
+                    >
+                        <div className="flex items-center">
+                            <TwitterIcon />
+                            <div className="ml-3">Twitter</div>
+                        </div>
+                        <ArrowIcon />
+                    </a>
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://github.com/mynhinguyentruong"
+                        className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
+                    >
+                        <div className="flex items-center">
+                            <GitHubIcon />
+                            <div className="ml-3">GitHub</div>
+                        </div>
+                        <ArrowIcon />
+                    </a>
+
+                </div>
+            </div>
+        </section>
+    );
 }
