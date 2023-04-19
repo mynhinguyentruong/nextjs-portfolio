40c44c76c24a2e7233ed34948eab8eef34a9ae7f
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-19 12:15:56 -0400

    Testing fetching delay and loading rendering

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,6 +1,8 @@
 // Test Next.js Loading UI
 import Loading from "./loading";
 import ReviewSkeleton from "../../components/review-skeleton";
+import {Suspense} from "react";
+import Review from "../../components/review";
 
 type Params = {
     params: {
@@ -14,11 +16,26 @@ export default async function Page({ params }: Params) {
 
     // await, fetch the data inside the component
 
+    // @ts-ignore
     return (
         <>
             <h1>Some dynamic data</h1>
             <Loading />
-            <ReviewSkeleton />
+            {/*<ReviewSkeleton />*/}
+            <Suspense fallback={<ReviewSkeleton/>}>
+                <Review
+                    data={fetch(
+                        // We intentionally delay the reponse to simulate a slow data
+                        // request that would benefit from streaming
+                        `https://app-dir.vercel.app/api/reviews?delay=5000`,
+                        {
+                            // We intentionally disable Next.js Cache to better demo
+                            // streaming
+                            cache: 'no-store',
+                        },
+                    )}
+                />
+            </Suspense>
         </>
 
     )
