2e8cd0205524722d6c74d5fd4ca41e24b1071789
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-05-22 09:52:05 -0400

    Add Initial Logo animation

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -23,72 +23,58 @@ const navItems = {
 function Logo() {
   return (
     <Link aria-label="Nhi Nguyen" href="/">
-
-      {/* To be implemented for the svg */}
-      <motion.svg fill="#000000" className="text-black dark:text-white h-[25px] md:h-[37px]" height="30px" width="40px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23.631 23.631">
-        <motion.g >
-          <motion.polygon  initial={{ pathLength: 0 }}
-                           animate={{ pathLength: 1 }}
-                           transition={{
-                             duration: 2,
-                             repeat: Infinity,
-                             repeatType: "loop",
-                             repeatDelay: 2
-                           }}
-                           points="0,0.663 9.401,0.663 15.882,7.146 15.882,14.127 5.307,3.608 5.274,22.969 0,22.969"
-
-                           strokeLinecap="round"
-                           strokeLinejoin="round"
-          >
-          </motion.polygon>
-          <motion.polygon  initial={{ pathLength: 0 }}
-                           animate={{ pathLength: 1 }}
-                           transition={{
-                             duration: 2,
-                             repeat: Infinity,
-                             repeatType: "loop",
-                             repeatDelay: 2
-                           }} points="23.631,22.969 14.232,22.969 7.752,16.485 7.752,9.501 18.327,20.018 18.359,0.662 23.631,0.662"></motion.polygon>
+     <motion.svg width="71" height="84" viewBox="0 0 120 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <motion.g filter="url(#filter0_d_2_23)">
+          <motion.path
+              initial={{
+            opacity: 0,
+            pathLength: 0,
+          }}
+            animate={{
+              opacity: 1,
+              pathLength: 1,
+            }} transition={{
+            duration: 2,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "loop",
+            repeatDelay: 1
+          }} d="M4 0.00341415H33.0412L53.0621 21.8581V45.3916L20.3942 9.93123L20.2922 75.1986H4V0.00341415Z"
+            stroke="currentColor"
+            strokeWidth={9}/>
+          <motion.path
+              initial={{
+                opacity: 0,
+                pathLength: 0,
+              }}
+              animate={{
+                opacity: 1,
+                pathLength: 1,
+              }}
+              transition={{
+                duration: 2,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatType: "loop",
+                repeatDelay: 1
+          }} d="M77 75.1986H47.965L27.9472 53.3405V29.7969L60.6151 65.2505L60.7139 0H77V75.1986Z"
+              stroke="currentColor"
+              strokeWidth={9}/>
         </motion.g>
+        <defs>
+          <filter id="filter0_d_2_23" x="0" y="0" width="81" height="83.1986" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+            <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2_23"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2_23" result="shape"/>
+          </filter>
+        </defs>
       </motion.svg>
-      {/*<motion.svg*/}
-      {/*  className="text-black dark:text-white h-[25px] md:h-[37px]"*/}
-      {/*  width="25"*/}
-      {/*  height="37"*/}
-      {/*  viewBox="0 0 23.631 23.631"*/}
-      {/*  fill="none"*/}
-      {/*  xmlns="http://www.w3.org/2000/svg"*/}
-      {/*>*/}
-      {/*  <motion.path*/}
-      {/*    initial={{*/}
-      {/*      opacity: 0,*/}
-      {/*      pathLength: 0,*/}
-      {/*    }}*/}
-      {/*    animate={{*/}
-      {/*      opacity: 1,*/}
-      {/*      pathLength: 1,*/}
-      {/*    }}*/}
-      {/*    transition={{*/}
-      {/*      duration: 0.5,*/}
-      {/*      type: 'spring',*/}
-      {/*      stiffness: 50,*/}
-      {/*    }}*/}
-      {/*    d="M39 316V0"*/}
-      {/*    stroke="currentColor"*/}
-      {/*    strokeWidth={78}*/}
-      {/*  />*/}
-      {/*  <motion.path*/}
-      {/*    initial={{ x: -200, opacity: 0 }}*/}
-      {/*    animate={{ x: 0, opacity: 1 }}*/}
-      {/*    transition={{*/}
-      {/*      duration: 0.5,*/}
-      {/*      type: 'spring',*/}
-      {/*      stiffness: 50,*/}
-      {/*    }}*/}
-      {/*    d="M232 314.998H129.852L232 232.887V314.998Z"*/}
-      {/*    fill="currentColor"*/}
-      {/*  />*/}
-      {/*</motion.svg>*/}
+
     </Link>
   );
 }
@@ -102,7 +88,7 @@ export default function Navbar() {
   return (
     <aside className="md:w-[150px] md:flex-shrink-0 -mx-4 md:mx-0 md:px-0 font-serif border-solid">
       <div className="lg:sticky lg:top-20 border-solid border-amber-500">
-        <div className="ml-2 md:ml-[12px] mb-2 px-4 md:px-0 md:mb-8 space-y-10 flex flex-col md:flex-row items-start ">
+        <div className="ml-1 md:ml-[12px] mb-2 px-4 md:px-0 md:mb-8 space-y-10 flex flex-col md:flex-row items-start ">
           <Logo />
         </div>
         <LayoutGroup>
